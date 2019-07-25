5c53475692231db592043d67910d3e0d38a05ed8
Dimitris Karathanasis <dkarathansis@dhcp-142-16.lls.for-the-inter.net>
2019-07-25 18:09:38 +0200

    Demo Ready

--- backend/core/taps/custom.py
+++ backend/core/taps/custom.py
@@ -3,40 +3,46 @@ from kombu import Exchange
 from kombu import Producer
 from utils import get_logger
 from utils import RABBITMQ_URI
+import sys
+import json
+import time
 
 log = get_logger()
 
 
 def run():
-    def runner(k):
-        msg_ = {
-            "timestamp": 1,
-            "orig_path": [],
-            "communities": [],
-            "service": "a",
-            "type": "A",
-            "path": [8, 3, 2, 1, ord(k)],
-            "prefix": "10.0.0.0/8",
-            "peer_asn": 8,
-        }
-        with Connection(RABBITMQ_URI) as connection:
-            exchange = Exchange(
-                "bgp-update", channel=connection, type="direct", durable=False
-            )
-            exchange.declare()
-            with Producer(connection) as producer:
-                for i in range(1000):
-                    msg_["timestamp"] = i
-                    msg_["key"] = "{}-{}".format(k, i)
-                    producer.publish(
-                        msg_, exchange=exchange, routing_key="update", serializer="json"
+    def runner():
+        with open(sys.argv[1]) as json_file:
+            data = json.load(json_file)
+            for i in range(7):
+                msg_ = {
+                    "timestamp": data['bgp_update'][i]['timestamp'],
+                    "orig_path": data['bgp_update'][i]['orig_path'],
+                    "communities": data['bgp_update'][i]['communities'],
+                    "service": data['bgp_update'][i]['service'],
+                    "type": data['bgp_update'][i]['type'],
+                    "path": data['bgp_update'][i]['path'],
+                    "prefix": data['bgp_update'][i]['prefix'],
+                    "peer_asn": data['bgp_update'][i]['peer_asn'],
+                }
+                with Connection(RABBITMQ_URI) as connection:
+                    exchange = Exchange(
+                        "bgp-update", channel=connection, type="direct", durable=False
                     )
+                    exchange.declare()
+                    with Producer(connection) as producer:
+                        msg_["key"] = "{}".format(i+50)
+                        print (msg_)
+                        producer.publish(
+                            msg_, exchange=exchange, routing_key="update", serializer="json"
+                        )
+                time.sleep(10)
 
     import threading
 
     threads = []
-    for i in range(10):
-        threads.append(threading.Thread(target=runner, args=(chr(i + 97),)))
+    for i in range(1):
+        threads.append(threading.Thread(target=runner,))
     for t in threads:
         t.start()
     for t in threads:
